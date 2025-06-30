656c974e6e39f92f414935d119349fb39a8955a3
FahimHasan683031 <fahimhasan683031@gmail.com>
2025-06-30 23:42:20 +0600

    upeat notification service

--- src/app/modules/notification/notification.service.ts
+++ src/app/modules/notification/notification.service.ts
@@ -13,40 +13,10 @@ import { Notification } from './notification.model';
 //   return Notifications
 // }
 
-// // get single Notification
-// const getSingleNotificationFromDB = async (id: string
-// ): Promise<Partial<INotification>> => {
-//   const isExistNotification = await Notification.findById(id)
-//   if (!isExistNotification) {
-//     throw new ApiError(StatusCodes.BAD_REQUEST, "Notification doesn't exist!");
-//   }
-
-//   return isExistNotification;
-// };
 
 
-// // update Notification
-// const updateNotificationToDB = async (
-//   id: string,
-//   payload: Partial<INotification>
-// ): Promise<Partial<INotification | null>> => {
-//   const isExistNotification = await Notification.findById(id);
-//   if (!isExistNotification) {
-//     throw new ApiError(StatusCodes.BAD_REQUEST, "Notification doesn't exist!");
-//   }
-
-//    const updateDoc = await Notification.findOneAndUpdate({ _id: id }, payload, {
-//     new: true,
-//   });
-
- 
-
-//   return updateDoc;
-
-// };
 
 
-// delete Notification
 const deleteNotification = async (
   id: string,
 ) => {
